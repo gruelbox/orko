ee178d796f1f3f6777b26daf23c0b5bf86d87fde
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-03-13 22:41:48 +0000

    Disable service worker 0 just making things more complicated for now. Use unrolled semantic UI CSS instead of the NPM package, so we can theme it later

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'semantic-ui-css/semantic.min.css';
+import './semantic/dist/semantic.min.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
+import unregister from './registerServiceWorker';
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
-registerServiceWorker();
+//registerServiceWorker();
+unregister();
