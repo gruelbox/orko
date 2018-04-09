24f54f1594933f5f37e614ae6ca0840e77643857
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-04-10 00:47:18 +0100

    Purge a load of old unused code

--- oco-ui/src/old/context/trade.js
+++
@@ -1,9 +0,0 @@
-import { put } from './fetchUtil';
-
-export const BUY = 'BUY';
-export const SELL = 'SELL';
-
-export function executeTrade(trade, auth) {
-  return put('jobs', auth.userName, auth.password, JSON.stringify(trade))
-    .then(auth.parseToJson);
-}
