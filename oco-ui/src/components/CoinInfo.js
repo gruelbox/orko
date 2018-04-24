1ad18363d9b08cafc2a777f4aa0c05339241597c
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-04-24 01:24:02 +0100

    Fixed the balances not displaying. Layout is still off though.

--- oco-ui/src/components/CoinInfo.js
+++ oco-ui/src/components/CoinInfo.js
@@ -3,102 +3,75 @@ import React from "react"
 import { Flex, Box } from "rebass"
 
 import Section from "./primitives/Section"
-import Warning from "./primitives/Warning"
 import PriceSet from "./primitives/PriceSet"
 import Price from "./primitives/Price"
-import Loading from "./primitives/Loading"
+
+const EMPTY = "--"
 
 export const CoinInfo = props => {
   const coin = props.coin
 
-  const noBalance = 
+  const noBalance =
     !props.balance || !props.balance[coin.base] || !props.balance[coin.counter]
 
-  var coinInfo
-  if (props.loading) {
-    coinInfo = <Loading />
-  } else {
-    coinInfo = (
-      <Flex flexWrap="wrap" justifyContent="space-between">
-        <Box width={[1/2, 1/4]}>
-          {!noBalance && (
-            <PriceSet>
-              <Price name={coin.base + " total"} onClick={props.onClickNumber}>
-                {props.balance[coin.base].total}
-              </Price>
-              <Price
-                name={coin.counter + " total"}
-                onClick={props.onClickNumber}
-              >
-                {props.balance[coin.counter].total}
-              </Price>
-            </PriceSet>
-          )}
-        </Box>
-        <Box width={[1/2, 1/4]}>
-          {!noBalance && (
-            <PriceSet>
-              <Price
-                name={coin.base + " available"}
-                onClick={props.onClickNumber}
-              >
-                {props.balance[coin.base].available}
-              </Price>
-              {props.ticker && (
-                <Price
-                  name={coin.base + " affordable"}
-                  onClick={props.onClickNumber}
-                >
-                  {props.balance[coin.counter].available /
-                    props.ticker.ask}
-                </Price>
-              )}
-              <Price
-                name={coin.counter + " available"}
-                onClick={props.onClickNumber}
-              >
-                {props.balance[coin.counter].available}
-              </Price>
-            </PriceSet>
-          )}
-        </Box>
-        <Box width={[1/2, 1/4]}>
-          {!props.ticker ? (
-            <Warning>Cannot fetch ticker</Warning>
-          ) : (
-            <PriceSet>
-              <Price name="Ask" onClick={props.onClickNumber}>
-                {props.ticker.ask}
-              </Price>
-              <Price name="Last" onClick={props.onClickNumber}>
-                {props.ticker.last}
-              </Price>
-              <Price name="Bid" onClick={props.onClickNumber}>
-                {props.ticker.bid}
-              </Price>
-              </PriceSet>
-          )}
-        </Box>
-        <Box width={[1/2, 1/4]}>
-          {!props.ticker ? (
-            <Warning>Cannot fetch ticker</Warning>
-          ) : (
-            <PriceSet>
-              <Price name="High" onClick={props.onClickNumber}>
-                {props.ticker.high}
-              </Price>
-              <Price name="Open" onClick={props.onClickNumber}>
-                {props.ticker.open}
-              </Price>
-              <Price name="Low" onClick={props.onClickNumber}>
-                {props.ticker.low}
-              </Price>
-              </PriceSet>
-          )}
-        </Box>
-      </Flex>
-    )
-  }
+  var coinInfo = (
+    <Flex flexWrap="wrap" justifyContent="space-between">
+      <Box width={[1 / 2, 1 / 4]}>
+        <PriceSet>
+          <Price name={coin.base + " total"} onClick={props.onClickNumber}>
+            {noBalance ? EMPTY : props.balance[coin.base].total}
+          </Price>
+          <Price name={coin.counter + " total"} onClick={props.onClickNumber}>
+            {noBalance ? EMPTY : props.balance[coin.counter].total}
+          </Price>
+        </PriceSet>
+      </Box>
+      <Box width={[1 / 2, 1 / 4]}>
+        <PriceSet>
+          <Price name={coin.base + " available"} onClick={props.onClickNumber}>
+            {noBalance ? EMPTY : props.balance[coin.base].available}
+          </Price>
+          <Price name={coin.base + " affordable"} onClick={props.onClickNumber}>
+            {noBalance || !props.ticker
+              ? EMPTY
+              : props.balance[coin.counter].available / props.ticker.ask}
+          </Price>
+          <Price
+            name={coin.counter + " available"}
+            onClick={props.onClickNumber}
+          >
+            {noBalance ? EMPTY : props.balance[coin.counter].available}
+          </Price>
+        </PriceSet>
+      </Box>
+      <Box width={[1 / 2, 1 / 4]}>
+        <PriceSet>
+          <Price name="Ask" onClick={props.onClickNumber}>
+            {props.ticker ? props.ticker.ask : EMPTY}
+          </Price>
+          <Price name="Last" onClick={props.onClickNumber}>
+            {props.ticker ? props.ticker.last : EMPTY}
+          </Price>
+          <Price name="Bid" onClick={props.onClickNumber}>
+            {props.ticker ? props.ticker.bid : EMPTY}
+          </Price>
+        </PriceSet>
+      </Box>
+      <Box width={[1 / 2, 1 / 4]}>
+        <PriceSet>
+          <Price name="High" onClick={props.onClickNumber}>
+            {props.ticker ? props.ticker.high : EMPTY}
+          </Price>
+          <Price name="Open" onClick={props.onClickNumber}>
+            {props.ticker ? props.ticker.open : EMPTY}
+          </Price>
+          <Price name="Low" onClick={props.onClickNumber}>
+            {props.ticker ? props.ticker.low : EMPTY}
+          </Price>
+        </PriceSet>
+      </Box>
+    </Flex>
+  )
 
   if (coin) {
     return (
--- oco-ui/src/containers/CoinInfoContainer.js
+++ oco-ui/src/containers/CoinInfoContainer.js
@@ -1,15 +1,31 @@
 import React from "react"
 import { connect } from "react-redux"
 import CoinInfo from "../components/CoinInfo"
+import * as coinActions from "../store/coin/actions"
+
+const TICK_TIME = 5000;
 
 class CoinInfoContainer extends React.Component {
+
+  tick = () => {
+    this.props.dispatch(coinActions.fetchBalance(this.props.coin));
+  }
+
+  componentDidMount() {
+    this.tick();
+    this.interval = setInterval(this.tick, TICK_TIME);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
       <CoinInfo
         coin={this.props.coin}
         balance={this.props.balance}
         ticker={this.props.tickers[this.props.coin.key]}
-        loading={false}
         onClickNumber={number => {
           if (this.props.updateFocusedField) {
             this.props.updateFocusedField(number)
