da05aadbcc5f9c0ff0b3568fd62b31d2cce7ed77
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-05-15 00:35:53 +0100

    Minimise rendering of ticker component

--- oco-ui/src/components/Ticker.js
+++ oco-ui/src/components/Ticker.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import styled from "styled-components"
 import Price from "./primitives/Price"
+import { areEqualShallow } from "../util/objectUtils"
 
 const Container = styled.div`
   display: flex;
@@ -10,33 +11,42 @@ const Container = styled.div`
   }
 `
 
-export const Ticker = props => {
-  const coin = props.coin
-  if (coin) {
-    return (
-      <Container>
-        <Price counter={coin.counter} name="Bid" nameColor="buy" icon="chevron up" onClick={props.onClickNumber}>
-          {props.ticker ? props.ticker.bid : undefined}
-        </Price>
-        <Price counter={coin.counter} name="Last" icon="circle outline" onClick={props.onClickNumber}>
-          {props.ticker ? props.ticker.last : undefined}
-        </Price>
-        <Price counter={coin.counter} name="Ask" nameColor="sell" icon="chevron down" onClick={props.onClickNumber}>
-          {props.ticker ? props.ticker.ask : undefined}
-        </Price>
-        <Price counter={coin.counter} name="Open" onClick={props.onClickNumber}>
-          {props.ticker ? props.ticker.open : undefined}
-        </Price>
-        <Price counter={coin.counter} name="24h Low" onClick={props.onClickNumber}>
-          {props.ticker ? props.ticker.low : undefined}
-        </Price>
-        <Price counter={coin.counter} name="24h High" onClick={props.onClickNumber}>
-          {props.ticker ? props.ticker.high : undefined}
-        </Price>
-      </Container>
-    )
-  } else {
-    return <div>No coin selected</div>
+class Ticker extends React.Component {
+
+  shouldComponentUpdate(nextProps) {
+    return !areEqualShallow(this.props, nextProps)
+  }
+
+  render() {
+    const coin = this.props.coin
+    const ticker = this.props.ticker
+    const onClickNumber = this.props.onClickNumber
+    if (coin) {
+      return (
+        <Container>
+          <Price counter={coin.counter} name="Bid" nameColor="buy" icon="chevron up" onClick={onClickNumber}>
+            {ticker ? ticker.bid : undefined}
+          </Price>
+          <Price counter={coin.counter} name="Last" icon="circle outline" onClick={onClickNumber}>
+            {ticker ? ticker.last : undefined}
+          </Price>
+          <Price counter={coin.counter} name="Ask" nameColor="sell" icon="chevron down" onClick={onClickNumber}>
+            {ticker ? ticker.ask : undefined}
+          </Price>
+          <Price counter={coin.counter} name="Open" onClick={onClickNumber}>
+            {ticker ? ticker.open : undefined}
+          </Price>
+          <Price counter={coin.counter} name="24h Low" onClick={onClickNumber}>
+            {ticker ? ticker.low : undefined}
+          </Price>
+          <Price counter={coin.counter} name="24h High" onClick={onClickNumber}>
+            {ticker ? ticker.high : undefined}
+          </Price>
+        </Container>
+      )
+    } else {
+      return <div>No coin selected</div>
+    }
   }
 }
 
