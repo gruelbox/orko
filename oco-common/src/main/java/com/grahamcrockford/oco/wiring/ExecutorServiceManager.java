6784d1ccc3bb7c225ed21273c13e61feabd3dfa4
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-05-24 00:21:29 +0100

    Name async event bus threads for debugging purposes

--- oco-common/src/main/java/com/grahamcrockford/oco/wiring/ExecutorServiceManager.java
+++ oco-common/src/main/java/com/grahamcrockford/oco/wiring/ExecutorServiceManager.java
@@ -1,8 +1,11 @@
 package com.grahamcrockford.oco.wiring;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 
 import com.google.inject.Singleton;
 
@@ -11,7 +14,19 @@ import io.dropwizard.lifecycle.Managed;
 @Singleton
 class ExecutorServiceManager implements Managed {
 
-  private final ExecutorService executor = Executors.newCachedThreadPool();
+  private final ExecutorService executor;
+
+  ExecutorServiceManager() {
+    BasicThreadFactory threadFactory = new BasicThreadFactory.Builder()
+        .namingPattern("AsyncEventBus-%d")
+        .daemon(false)
+        .priority(Thread.NORM_PRIORITY)
+        .build();
+    executor = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+        30L, TimeUnit.SECONDS,
+        new SynchronousQueue<Runnable>(),
+        threadFactory);
+  }
 
   @Override
   public void start() throws Exception {
