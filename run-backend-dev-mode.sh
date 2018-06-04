e39b52c91a2837616ac9f51a3bb8794496b74756
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-06-04 18:57:46 +0100

    Add useful script for launching dev mode backend

---
+++ run-backend-dev-mode.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp oco-all-in-one/target/classes:oco-all-in-one/target/dependency/* com.grahamcrockford.oco.allinone.AllInOneApplication server ./example-developer-mode-config-allinone.yml
