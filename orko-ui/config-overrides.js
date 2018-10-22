46346b50906a003d9271c1f1fe3c28aa8f9e3a6b
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-10-22 16:25:31 +0100

    Revert "Re-instate sockets and fix HME error using react-app-rewired"
    This reverts commit e72d35de0a912c3554d7ecd84e286e1ceda83d24.

--- orko-ui/config-overrides.js
+++
@@ -1,8 +0,0 @@
-module.exports = function override(config, env) {
-  /*   config.module.rules.push({
-    test: /\.worker\.js$/,
-    use: { loader: "worker-loader" }
-  })
-  config.output.globalObject = "this" */
-  return config
-}
