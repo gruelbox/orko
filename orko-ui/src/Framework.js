42a967a76f421b3c4f73229714da2d3b83d64f94
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-11-04 19:58:08 +0000

    Tweak panel widths

--- orko-ui/src/Framework.js
+++ orko-ui/src/Framework.js
@@ -79,18 +79,18 @@ const baseLayouts = Immutable({
 
     { i: "marketData", x: 13, y: 100, w: 7, h: 11 },
     { i: "openOrders", x: 13, y: 200, w: 7, h: 11 },
-    { i: "jobs", x: 13, y: 300, w: 7, h: 9 },
+    { i: "jobs", x: 13, y: 300, w: 7, h: 9 }
   ],
   md: [
     { i: "chart", x: 0, y: 100, w: 10, h: 13 },
     { i: "openOrders", x: 0, y: 200, w: 10, h: 5 },
     { i: "balance", x: 0, y: 300, w: 10, h: 4 },
     { i: "tradeSelector", x: 0, y: 400, w: 10, h: 9 },
-    
-    { i: "coins", x: 5, y: 100, w: 6, h: 11 },
-    { i: "marketData", x: 5, y: 200, w: 6, h: 8 },
-    { i: "jobs", x: 5, y: 300, w: 6, h: 5 },
-    { i: "notifications", x: 5, y: 400, w: 6, h: 7 }
+
+    { i: "coins", x: 10, y: 100, w: 6, h: 11 },
+    { i: "marketData", x: 10, y: 200, w: 6, h: 8 },
+    { i: "jobs", x: 10, y: 300, w: 6, h: 5 },
+    { i: "notifications", x: 10, y: 400, w: 6, h: 7 }
   ],
   sm: [
     { i: "chart", x: 0, y: 100, w: 2, h: 12 },
