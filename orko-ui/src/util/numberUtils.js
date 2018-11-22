36ae3f0972ae848dac2cc3c31161698bca3a12ca
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-11-22 23:47:55 +0000

    Fix some glitches with the market trades view

--- orko-ui/src/util/numberUtils.js
+++ orko-ui/src/util/numberUtils.js
@@ -2,12 +2,16 @@ export const isValidNumber = val => !isNaN(val) && val !== "" && val !== null
 
 export const formatNumber = (x, scale, undefinedValue) => {
   if (!isValidNumber(x)) return undefinedValue
+  const negative = x < 0
   if (scale < 0) {
-    const split = x.toString().split("-")
+    const split = negative
+      ? (-x).toString().split("-")
+      : x.toString().split("-")
     if (split.length > 1) {
-      return Number(x).toFixed(split[1])
+      var result = Number(x).toFixed(split[1])
+      return negative ? -result : result
     } else {
-      return split[0]
+      return negative ? -split[0] : split[0]
     }
   } else {
     return Number(x).toFixed(scale)
