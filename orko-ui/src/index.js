93458ff48a68e105905582da3a4376738e7dd7e7
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-12-20 00:27:14 +0000

    Semantic theming

--- orko-ui/src/index.js
+++ orko-ui/src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import "./semantic/dist/semantic.min.css"
 import "./index.css"
-import "semantic-ui-css/semantic.min.css"
 import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
