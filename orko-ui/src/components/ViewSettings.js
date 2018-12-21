2041b0efea93bc3ed0d8b9119f1afbb08fd30e3b
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-12-21 17:23:30 +0000

    Switch view settings to use Semantic

--- orko-ui/src/components/ViewSettings.js
+++ orko-ui/src/components/ViewSettings.js
@@ -1,33 +1,22 @@
 import React, { Component } from "react"
 
-import Section from "./primitives/Section"
-import Modal from "./primitives/Modal"
-import Href from "./primitives/Href"
-import { Icon } from "semantic-ui-react"
-import Form from "./primitives/Form"
-import Button from "./primitives/Button"
-import FormButtonBar from "./primitives/FormButtonBar"
-import Checkbox from "./primitives/Checkbox"
+import FixedModal from "./primitives/FixedModal"
+import { Icon, Modal, Button, Form } from "semantic-ui-react"
 import Immutable from "seamless-immutable"
 
 export default class ViewSettings extends Component {
   render() {
     return (
-      <Modal>
-        <Section
-          id="viewSettings"
-          heading={"View settings"}
-          buttons={() => (
-            <Href title="Close" onClick={() => this.props.onClose()}>
-              <Icon fitted name="close" />
-            </Href>
-          )}
-        >
+      <FixedModal closeIcon onClose={this.props.onClose}>
+        <Modal.Header id="viewSettings">
+          <Icon name="eye" />
+          View settings
+        </Modal.Header>
+        <Modal.Content>
           <Form>
             {this.props.panels.map(panel => (
-              <Checkbox
+              <Form.Checkbox
                 key={panel.key}
-                id={"panel" + panel.key}
                 label={panel.title}
                 type="checkbox"
                 checked={panel.visible}
@@ -38,13 +27,15 @@ export default class ViewSettings extends Component {
                 }
               />
             ))}
-            <FormButtonBar>
-              <Button onClick={this.props.onReset}>Reset</Button>
-              <Button onClick={this.props.onClose}>OK</Button>
-            </FormButtonBar>
           </Form>
-        </Section>
-      </Modal>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={this.props.onReset}>Reset</Button>
+          <Button onClick={this.props.onClose} primary>
+            OK
+          </Button>
+        </Modal.Actions>
+      </FixedModal>
     )
   }
 }
