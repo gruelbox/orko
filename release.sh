4173811cd356249fd477072482ca8697bec584c6
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-01-02 18:42:40 +0000

    Adjustments to the release build

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-mvn -Prelease --batch-mode -Dtag=0.8.5 release:prepare \
-                 -DreleaseVersion=0.8.5 \
-                 -DdevelopmentVersion=0.8.6-SNAPSHOT \
-                 -DskipTests=true
+mvn release:prepare \
+    -Dtag=0.8.5  \
+    -DpreparationGoals=clean \
+    -DautoVersionSubmodules=true
+    -Prelease \
+    -Darguments='-Prelease'
