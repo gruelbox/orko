932bcb9cab6f77b379725e432379966eb3088df6
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-01-02 22:28:59 +0000

    Fix missing linefeed, add commit after licence setup

--- release.sh
+++ release.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 ./licences.sh
+git add -A
+git diff-index --quiet HEAD || git commit -m "Update licence headers"
 mvn release:prepare \
     -DpreparationGoals=clean \
-    -DautoVersionSubmodules=true
+    -DautoVersionSubmodules=true \
     -Pintegration-test
