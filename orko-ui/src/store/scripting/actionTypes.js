498ada02318a9a19a9edd209e705e099834288f8
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-01-02 21:05:03 +0000

    Remove all the mastheads

--- orko-ui/src/store/scripting/actionTypes.js
+++ orko-ui/src/store/scripting/actionTypes.js
@@ -1,23 +1,3 @@
-/*-
- * ===============================================================================L
- * Orko UI
- * ================================================================================
- * Copyright (C) 2018 - 2019 Graham Crockford
- * ================================================================================
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * ===============================================================================E
- */
 export const SET_SCRIPTS = "scripting.SET_SCRIPTS"
 export const ADD_SCRIPT = "scripting.ADD_SCRIPT"
 export const DELETE_SCRIPT = "scripting.DELETE_SCRIPT"
