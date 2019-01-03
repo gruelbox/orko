9bce58585814f7fde0a70a4d411c1bea89bd2ea5
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-01-03 00:39:26 +0000

    Fix version test so it works in all cases

--- orko-common/src/test/java/com/gruelbox/orko/support/TestRealVersion.java
+++ orko-common/src/test/java/com/gruelbox/orko/support/TestRealVersion.java
@@ -1,14 +1,36 @@
 package com.gruelbox.orko.support;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class TestRealVersion {
 
+  private static final String VALID_VERSION = "(\\d)+\\.(\\d)+\\.(\\d)+(-SNAPSHOT)?";
+
+  @Test
+  public void testVerifyCheckingRegex() {
+    assertFalse("".matches(VALID_VERSION));
+    assertFalse("1".matches(VALID_VERSION));
+    assertFalse("0.1".matches(VALID_VERSION));
+    assertFalse("0.1.".matches(VALID_VERSION));
+    assertFalse("0.1.2-".matches(VALID_VERSION));
+    assertFalse("0.1.2-SNAPSHOT-1".matches(VALID_VERSION));
+
+    assertTrue("0.1.2".matches(VALID_VERSION));
+    assertTrue("0.1.2-SNAPSHOT".matches(VALID_VERSION));
+    assertTrue("0.14.21".matches(VALID_VERSION));
+    assertTrue("1234.23.1234".matches(VALID_VERSION));
+    assertTrue("1234.23.1234-SNAPSHOT".matches(VALID_VERSION));
+  }
+
   @Test
   public void testGetVersion() {
-    assertEquals("Development version", ReadVersion.readVersionInfoInManifest());
+    assertTrue(
+      ReadVersion.readVersionInfoInManifest().equals("${project.version}") ||
+      ReadVersion.readVersionInfoInManifest().matches(VALID_VERSION)
+    );
   }
 
 }
