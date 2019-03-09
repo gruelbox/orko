0d090fad30bcc77349572cc91a282c60b938e56d
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-03-09 15:32:41 +0000

    Post toStrings of execution reports To get to the bottom of sign-switching on streamed trades

--- orko-common/src/main/java/com/gruelbox/orko/strategy/StrategyModule.java
+++ orko-common/src/main/java/com/gruelbox/orko/strategy/StrategyModule.java
@@ -27,5 +27,6 @@ public class StrategyModule extends AbstractModule {
   @Override
   protected void configure() {
     Multibinder.newSetBinder(binder(), Managed.class).addBinding().to(UserTradeNotifier.class);
+    Multibinder.newSetBinder(binder(), Managed.class).addBinding().to(ExecutionReportLogger.class);
   }
 }
