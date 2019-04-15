03fd33ce335fe42d4beaa63af65d31e12bd1902d
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-04-15 23:05:54 +0100

    Fix warnings

--- orko-common/src/main/java/com/gruelbox/orko/job/script/ScriptJobProcessor.java
+++ orko-common/src/main/java/com/gruelbox/orko/job/script/ScriptJobProcessor.java
@@ -75,7 +75,6 @@ import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
  *
  * @author Graham Crockford
  */
-@SuppressWarnings("restriction")
 class ScriptJobProcessor implements ScriptJob.Processor {
 
   private static final String PERMANENTLY_FAILED = "' permanently failed: ";
