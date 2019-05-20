542b10f4fbf78f02180620b1a4f4a6d10cca9905
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-05-20 21:45:16 +0100

    Remote install

--- build-backend-only.sh
+++ build-backend-only.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-mvn clean package -U -Pwebdev -Dmaven.test.skip=true -Dskip.failsafe.tests=true -Dspotbugs.skip=true -Dcheckstyle.skip=true -T 1C
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+mvn clean package -f "$DIR" -U -Pwebdev -Dmaven.test.skip=true -Dskip.failsafe.tests=true -Dspotbugs.skip=true -Dcheckstyle.skip=true -T 1C
