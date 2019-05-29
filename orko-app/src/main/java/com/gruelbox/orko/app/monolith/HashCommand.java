0307c40d1159f6cca0518bc38a771225cc9ffec4
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-05-29 22:01:27 +0100

    Sonar fix

--- orko-app/src/main/java/com/gruelbox/orko/app/monolith/HashCommand.java
+++ orko-app/src/main/java/com/gruelbox/orko/app/monolith/HashCommand.java
@@ -27,6 +27,9 @@ import net.sourceforge.argparse4j.inf.Subparser;
 
 class HashCommand extends Command {
 
+  private static final String SALT_PARM = "salt";
+  private static final String VALUE_PARM = "value";
+
   HashCommand() {
     super("hash", "Hashes the specified value using a provided salt");
   }
@@ -34,21 +37,21 @@ class HashCommand extends Command {
   @Override
   public void run(Bootstrap<?> bootstrap, Namespace namespace) throws Exception {
     Hasher hasher = new Hasher();
-    String salt = namespace.getString("salt");
+    String salt = namespace.getString(SALT_PARM);
     if (salt == null) {
       salt = hasher.salt();
       System.out.println("Salt used: " + salt);
-      System.out.println("Hashed result: " + hasher.hash(namespace.getString("value"), salt));
+      System.out.println("Hashed result: " + hasher.hash(namespace.getString(VALUE_PARM), salt));
     } else {
-      System.out.print(hasher.hash(namespace.getString("value"), salt));
+      System.out.print(hasher.hash(namespace.getString(VALUE_PARM), salt));
     }
   }
 
   @Override
   public void configure(Subparser subparser) {
-    subparser.addArgument("--salt", "-s")
+    subparser.addArgument("--" + SALT_PARM, "-s")
       .help("An encryption salt. If not provided, a new one will be used and returned.");
-    subparser.addArgument("value")
+    subparser.addArgument(VALUE_PARM)
       .required(true)
       .help("The value for which to create a hash.");
   }
