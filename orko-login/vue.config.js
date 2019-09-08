a36644951c491d26da2dc3f15942acd50349774f
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-09-08 13:44:48 +0100

    Remove Vue project

--- orko-login/vue.config.js
+++
@@ -1,9 +0,0 @@
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-
-module.exports = {
-  configureWebpack: {
-    resolve: {
-      plugins: [new TsconfigPathsPlugin()]
-    }
-  }
-};
