5865b1cf2b4964836f9f1224be2e77d8d94fbff3
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-09-21 23:02:37 +0100

    Remove redundant login app

--- orko-login/src/App.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import Authoriser from "containers/Authoriser"
-
-const App = () => <Authoriser />
-export default App
--- orko-login/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "@orko-semantic/semantic.min.css"
-import App from "./App"
-
-ReactDOM.render(<App />, document.getElementById("root"))
