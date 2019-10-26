a3713a5b360dc8fa99cd6eede7860a9a3ded2e92
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-10-26 18:58:59 +0100

    Ditch Spotbugs. It never finds anything useful.

--- build-backend-only.sh
+++ build-backend-only.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-mvn clean package -f "$DIR" -U -Pbundle -Dmaven.test.skip=true -Dskip.failsafe.tests=true -Dspotbugs.skip=true -Dcheckstyle.skip=true -T 1C
+mvn clean package -f "$DIR" -U -Pbundle -Dmaven.test.skip=true -Dskip.failsafe.tests=true -Dcheckstyle.skip=true -T 1C
