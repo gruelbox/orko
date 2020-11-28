2f3f37e81cede6d5f154e96b267df68a4f53cf64
Graham Crockford <badgerwithagun@users.noreply.github.com>
2020-11-28 22:05:40 +0000

    Updated semantic source

--- orko-semantic/gulpfile.js
+++ orko-semantic/gulpfile.js
@@ -2,28 +2,34 @@
  *           Set-up
  *******************************/
 
-var gulp = require("gulp"),
+var
+  gulp   = require('gulp'),
+
   // read user config to know what task to load
-  config = require("./tasks/config/user")
+  config = require('./tasks/config/user')
+;
+
+
 /*******************************
  *            Tasks
  *******************************/
 
-require("./tasks/collections/build")(gulp)
-require("./tasks/collections/install")(gulp)
+require('./tasks/collections/build')(gulp);
+require('./tasks/collections/various')(gulp);
+require('./tasks/collections/install')(gulp);
 
-gulp.task("default", gulp.series("watch"))
+gulp.task('default', gulp.series('watch'));
 
 /*--------------
       Docs
 ---------------*/
 
-require("./tasks/collections/docs")(gulp)
+require('./tasks/collections/docs')(gulp);
 
 /*--------------
       RTL
 ---------------*/
 
 if (config.rtl) {
-  require("./tasks/collections/rtl")(gulp)
+  require('./tasks/collections/rtl')(gulp);
 }
